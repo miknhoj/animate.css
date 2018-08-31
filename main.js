5a1980a6386fe596706ad6cf3cbd84a0074d4ce1
miknhoj <kimjohan86@gmail.com>
2018-08-31 13:38:13 -0400

    add reference link

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
 $(function () {
 
-    $(function () {
-        $('#bounce-btn').click(function () {
-            animate('#head-2', 'bounce');
-            // return false;
-        });
+    //     $(function () {
+    //         $('#bounce-btn').click(function () {
+    //             animate('#head-2', 'bounce');
+    //             // return false;
+    //         });
 
-        $('#jello-btn').click(function () {
-            animate('#head-3', 'jello');
-            // return false;
-        });
+    //         $('#jello-btn').click(function () {
+    //             animate('#head-3', 'jello');
+    //             // return false;
+    //         });
 
 
-        function animate(element, animation) {
-            $(element).addClass('animated ' + animation);
-            var wait = setTimeout(function () {
-                $(element).removeClass('animated ' + animation)
-            }, 1000);
-        }
+    //         function animate(element, animation) {
+    //             $(element).addClass('animated ' + animation);
+    //             var wait = setTimeout(function () {
+    //                 $(element).removeClass('animated ' + animation)
+    //             }, 1000);
+    //         }
+    //     })
+    // })
+
+    //Alternate button funcations/
+    $('#bounce-btn').on('click', function () {
+        $('#head-2').addClass('animated bounce')
+        setTimeout(function () {
+            $('#head-2').removeClass('animated bounce')
+        }, 1000)
+    })
+
+    $('#jello-btn').on('click', function () {
+        $('#head-3').addClass('animated jello')
+        setTimeout(function () {
+            $('#head-3').removeClass('animated jello')
+        }, 1000)
     })
 })
 
-// $('#jello-btn').on('click', function () {
-//     $('#section-3').addClass('animated jello')
-//     setTimeout(function (){ 
-//         $('#section-3').removeClass('animated jello')
-//   }, 3000)
-// })
+$('#more-btn').on('click', function () {
+    window.open('https://daneden.github.io/animate.css/');
 
-// $('#slide-in-right').on('click', function () {
-//     $('#anim-head').addClass('animated slideInRight')
-//     setTimeout(function (){ 
-//         $('#anim-head').removeClass('animated jello')
-//   }, 3000)
-// })
+})
 
 
